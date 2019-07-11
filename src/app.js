e825456720f953cbd7932ee1b97abfd3ba5f6b8c
Eddy Caldas <eddycaldas@live.com>
2019-07-11 11:59:17 -0600

    install react and react-dom

--- src/app.js
+++ src/app.js
@@ -1,2 +1,8 @@
-class Human {}
+import React from 'react'
+import ReactDOM from 'react-dom'
 
+const App = () => {
+    return <h1>This is a component APP</h1>
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'))
